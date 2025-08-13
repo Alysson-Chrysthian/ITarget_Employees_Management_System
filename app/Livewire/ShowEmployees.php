714e021922766d4acf5388f84c6e089af3088aa7
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-08-13 19:00:04 -0400

    Search method done

--- app/Livewire/ShowEmployees.php
+++ app/Livewire/ShowEmployees.php
@@ -28,7 +28,15 @@ class ShowEmployees extends Component
     #[Layout('components.layouts.main')]
     public function render()
     {
-        $employees = Employee::all();
+        $employees = Employee::where('id', 'like', '%' . $this->search . '%')
+            ->orWhere('name', 'like', '%' . $this->search . '%')
+            ->orWhere('email', 'like', '%' . $this->search . '%')
+            ->orWhere('cpf', 'like', '%' . $this->search . '%')
+            ->orWhere('cep', 'like', '%' . $this->search . '%')
+            ->orWhere('street', 'like', '%' . $this->search . '%')
+            ->orWhere('linguee', 'like', '%' . $this->search . '%')
+            ->orWhere('registration', 'like', '%' . $this->search . '%')
+            ->get();
 
         return view('livewire.show-employees', [
             'employees' => $employees,
